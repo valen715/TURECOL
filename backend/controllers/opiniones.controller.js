590926e1cbe5af1d29640b130ecb3188bccc3613
Valentina Restrepo <Vrestrepomontoya@gmail.com>
2021-08-29 23:04:20 -0500

    Se continua

---
+++ backend/controllers/opiniones.controller.js
@@ -0,0 +1,9 @@
+const Opinion = require('../models/opinionesEntity');
+// Operadores de bd como OR, AND, etc...
+const { Op } = require("sequelize");
+
+const opinionController = {};
+
+opinionController.crearComentario = async (req, res) => {
+    
+}
