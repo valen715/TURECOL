35f6e1d99a9ba86450af262a2e61575ed0111046
valen715 <Vrestrepomontoya@gmail.com>
2022-02-10 22:16:19 -0500

    Test

--- backend/test/Users.test.js
+++ backend/test/Users.test.js
@@ -1,17 +1,20 @@
-const supertest = require( "supertest");
-const app = require( "../app");
+const supertest = require("supertest");
+const app = require("../app");
+const { loginSencillo } = require("../controllers/usuarios.controller");
 
 const request = supertest.agent(app);
 
-
-describe('', () => {
-    it('login Usuario', (done) =>{
-        request.post('/usuarios/login-sencillo')
-        .send({correo: "ejemplo@eso.com", clave:"Admin1234*"} )
-        .expect(200)
-        .end(function(err, res) {
+describe("", () => {
+  it("login Usuario", (done) => {
+    request
+      .post("/usuarios/login-sencillo")
+      .send({ correo: "ejemplo@eso.com", clave: "Admin1234*" })
+      .expect(200)
+      .end(function (err, res) {
         if (err) return done(err);
         return done();
+
+        console.info("Usuario logueado exitosamente");
       });
-    })
-})
+  });
+});
