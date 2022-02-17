674f8a616805071e674ebf10832d2d84b2203bef
valen715 <Vrestrepomontoya@gmail.com>
2022-02-17 00:07:18 -0500

    Final pruebas unitarias

--- backend/test/Users.test.js
+++ backend/test/Users.test.js
@@ -4,8 +4,8 @@ const { loginSencillo } = require("../controllers/usuarios.controller");
 
 const request = supertest.agent(app);
 
-describe("", () => {
-  it("login Usuario", (done) => {
+describe("Login", () => {
+  it("login correcto", (done) => {
     request
       .post("/usuarios/login-sencillo")
       .send({ correo: "ejemplo@eso.com", clave: "Admin1234*" })
@@ -13,8 +13,40 @@ describe("", () => {
       .end(function (err, res) {
         if (err) return done(err);
         return done();
-
-        console.info("Usuario logueado exitosamente");
       });
   });
+
+  it("login invalido", (done) => {
+    request
+      .post("/usuarios/login-sencillo")
+      .send({ correo: "hola@eso.com", clave: "hola123*" })
+      .expect(204)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
 });
+});
+
+describe("Registro", () => {
+  it("Registro correcto", (done) => {
+    request
+      .post("/usuarios/registrarUsuario")
+      .send({ nombre: "pepita", correo: `${Math.random()}pepe@hola.com`, clave: "pepito1234*" })
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  it("Registro invalido", (done) => {
+    request
+      .post("/usuarios/registrarUsuario")
+      .send({ nombre: "pepito", correo: "pepito@eso.com", clave: "pepito1234*" })
+      .expect(409)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+}); 
